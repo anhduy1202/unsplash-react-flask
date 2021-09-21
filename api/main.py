1cff4dc7929c2c21ac645f1d03cc55c09d5f64b8
danieltruong <aduy1122@gmail.com>
2021-09-21 02:02:10 -0700

    /new-image route with query for random image & environment variable

--- api/main.py
+++ api/main.py
@@ -1,11 +1,40 @@
-from flask import Flask
- 
+import requests
+import os
+from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+
+# check error
+if not UNSPLASH_KEY:  # If key is empty
+    raise EnvironmentError(
+        "Please create .env.local file and insert UNSPLASH_KEY")
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-     return "Hello me!"
 
-#Use this to run flask in terminal by python main.y command
+@app.route("/new-image")
+def new_image():
+
+    # read the word next to query ex" /new-image?query="car"
+    word = request.args.get("query")
+
+    # Construct the headers based on UNSPLASH documentation
+    headers = {
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
+    }
+    # params to set in the request
+    params = {
+        "query": word
+    }
+    # Get method
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
+
+
+# Use this to run flask in terminal by python main.y command
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050)
