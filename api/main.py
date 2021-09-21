b08ebc5e2068309985dddc21223b2107d3e48e52
danieltruong <aduy1122@gmail.com>
2021-09-21 00:07:52 -0700

    Sample flask app

--- api/main.py
+++ api/main.py
@@ -1,1 +1,11 @@
-print("Hello from python")
+from flask import Flask
+ 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+     return "Hello me!"
+
+#Use this to run flask in terminal by python main.y command
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=5050)
