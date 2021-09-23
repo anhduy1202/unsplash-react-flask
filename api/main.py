6b9cd0279db100307a384acd4f0431313d8dec27
danieltruong <aduy1122@gmail.com>
2021-09-23 00:59:55 -0700

    debug mode: on

--- api/main.py
+++ api/main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env.local")
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 # check error
 if not UNSPLASH_KEY:  # If key is empty
@@ -13,6 +14,8 @@ if not UNSPLASH_KEY:  # If key is empty
         "Please create .env.local file and insert UNSPLASH_KEY")
 app = Flask(__name__)
 
+#Enable debug mode
+app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
@@ -35,6 +38,6 @@ def new_image():
     return data
 
 
-# Use this to run flask in terminal by python main.y command
+# Use this to run flask in terminal by python main.py command
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
